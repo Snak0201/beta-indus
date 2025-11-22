c47eb7326bbaf2a38bbf0fd35d9e50b0b64a8200
Snak-G23 <snak.gatepia@gmail.com>
2025-11-23 02:02:34 +0900

    モデルにバリデーションを記載する

--- app/models/birthday/brand.rb
+++ app/models/birthday/brand.rb
@@ -1,2 +1,3 @@
 class Birthday::Brand < ApplicationRecord
+  validates :name, :name_kana, :formal_name, :formal_name_kana, :born_on, presence: true
 end
--- app/models/birthday/character.rb
+++ app/models/birthday/character.rb
@@ -1,2 +1,3 @@
 class Birthday::Character < ApplicationRecord
+  validates :last_name, :first_name, :last_name_kana, :first_name_kana, :born_on, presence: true
 end
