77373bf6f5c009c1c01c170f32f182a116ce6715
Snak-G23 <snak.gatepia@gmail.com>
2025-11-22 17:21:45 +0900

    footerの作成

---
+++ test/components/footer_component_test.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class FooterComponentTest < ViewComponent::TestCase
+  def test_component_includes_title
+    render_inline(FooterComponent.new)
+
+    assert_selector "footer.footer span.text", text: "βインディの星明かり"
+  end
+end
