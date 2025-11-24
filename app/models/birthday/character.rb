2c1b543115d797670e3de2cbfa02092676069f96
Snak-G23 <snak.gatepia@gmail.com>
2025-11-24 17:48:23 +0900

    日付順に並べる

--- app/models/birthday/character.rb
+++ app/models/birthday/character.rb
@@ -2,4 +2,10 @@ class Birthday::Character < ApplicationRecord
   belongs_to :brand, class_name: "Birthday::Brand", foreign_key: "birthday_brand_id", optional: true
 
   validates :last_name, :first_name, :last_name_kana, :first_name_kana, :born_on, presence: true
+
+  def self.nearer_birthday
+    select(
+      "strftime('%m/%d', date(born_on)) AS date, *"
+    ).order(date: :asc)
+  end
 end
