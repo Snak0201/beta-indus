9e59b20fba7054908e987ddd8b521cb686b4b364
Snak-G23 <snak.gatepia@gmail.com>
2025-11-24 22:17:29 +0900

    モデルのスコープとメソッドを追加

--- app/models/birthday/character.rb
+++ app/models/birthday/character.rb
@@ -3,11 +3,15 @@ class Birthday::Character < ApplicationRecord
 
   validates :last_name, :first_name, :last_name_kana, :first_name_kana, :born_on, presence: true
 
-  def self.nearer_birthday
+  scope :nearer_birthday, -> {
     select(
       "strftime('%m/%d', date(born_on)) AS born_on_date, 
        strftime('%m%d', date(born_on)) < strftime('%m%d', date('now')) AS passed, 
        *"
     ).order(passed: :asc, born_on_date: :asc).limit(5)
+  }
+
+  def full_name
+    "#{last_name}#{first_name}"
   end
 end
