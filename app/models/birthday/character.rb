b793043ff3d710e34f1039ec43dd251b9dd7b701
Snak-G23 <snak.gatepia@gmail.com>
2025-11-24 18:12:18 +0900

    期近の誕生日の並び替えロジックを作成

--- app/models/birthday/character.rb
+++ app/models/birthday/character.rb
@@ -5,7 +5,9 @@ class Birthday::Character < ApplicationRecord
 
   def self.nearer_birthday
     select(
-      "strftime('%m/%d', date(born_on)) AS date, *"
-    ).order(date: :asc)
+      "strftime('%m/%d', date(born_on)) AS born_on_date, 
+       strftime('%m%d', date(born_on)) < strftime('%m%d', date('now')) AS passed, 
+       *"
+    ).order(passed: :asc, born_on_date: :asc).limit(5)
   end
 end
