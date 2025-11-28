0ebd259fed99ec16ac4d27e03be35eb8ffd142da
Snak-G23 <snak.gatepia@gmail.com>
2025-11-29 02:21:56 +0900

    表示の修正

--- app/components/sidebar_component.rb
+++ app/components/sidebar_component.rb
@@ -3,5 +3,6 @@
 class SidebarComponent < ViewComponent::Base
   def initialize
     @nearer_birthday_characters = Birthday::Character.nearer_birthday
+    @nearer_birthday_brands = Birthday::Brand.nearer_birthday
   end
 end
