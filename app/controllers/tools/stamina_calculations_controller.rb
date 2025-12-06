ef45131cde93d451f164d7e25307e7a923c02233
Snak-G23 <snak.gatepia@gmail.com>
2025-12-06 23:15:06 +0900

    flash名を変更する

--- app/controllers/tools/stamina_calculations_controller.rb
+++ app/controllers/tools/stamina_calculations_controller.rb
@@ -1,22 +1,22 @@
-class Tools::StaminaCalculationsController < ApplicationController
-  def show
-    @stamina_calculation = Tools::StaminaCalculation.new
-  end
+  class Tools::StaminaCalculationsController < ApplicationController
+    def show
+      @stamina_calculation = Tools::StaminaCalculation.new
+    end
 
-  def create
-    @stamina_calculation = Tools::StaminaCalculation.new(stamina_calculation_params)
-    if @stamina_calculation.valid?
-      flash.now[:success] = "スタミナ回復時間を計算しました。"
-      render :show
-    else
-      flash.now[:error] = "入力に誤りがあります。"
-      render :show, status: :unprocessable_entity
+    def create
+      @stamina_calculation = Tools::StaminaCalculation.new(stamina_calculation_params)
+      if @stamina_calculation.valid?
+        flash.now[:notice] = "スタミナ回復時間を計算しました。"
+        render :show
+      else
+        flash.now[:alert] = "入力に誤りがあります。"
+        render :show, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def stamina_calculation_params
+      params.require(:tools_stamina_calculation).permit(:target_stamina, :current_stamina, :recovery_seconds)
     end
   end
-
-  private
-
-  def stamina_calculation_params
-    params.require(:tools_stamina_calculation).permit(:target_stamina, :current_stamina, :recovery_seconds)
-  end
-end
