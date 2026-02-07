764f048274cda041991dc8b8234ea5344c70d4d5
Snak-G23 <snak.gatepia@gmail.com>
2026-02-07 13:35:32 +0900

    テストを追加する

--- test/models/tools/dice_test.rb
+++ test/models/tools/dice_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class Tools::DiceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "roll should return values with valid params" do
+    dice = Tools::Dice.new(number: 100, side: 3)
+    result = dice.roll
+
+    assert_equal result.length, 100
+    assert_empty result - [ 1, 2, 3 ]
+  end
+
+  test "should invalid with invalid params" do
+    dice = Tools::Dice.new(number: "", side: 0)
+
+    assert_not dice.valid?
+    assert_includes dice.errors[:number], "は数値で入力してください"
+    assert_includes dice.errors[:side], "は0より大きい値にしてください"
+  end
 end
