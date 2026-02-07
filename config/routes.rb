932031b28dd7a999a4eb5dac3b95592bc619dea6
Snak-G23 <snak.gatepia@gmail.com>
2026-02-07 21:37:36 +0900

    認証機能を追加する

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
   root "static_pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_22_160322) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_07_123621) do
   create_table "birthday_brands", force: :cascade do |t|
     t.date "born_on", null: false
     t.string "color"
@@ -35,5 +35,23 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_22_160322) do
     t.index ["birthday_brand_id"], name: "index_birthday_characters_on_birthday_brand_id"
   end
 
+  create_table "sessions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "ip_address"
+    t.datetime "updated_at", null: false
+    t.string "user_agent"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email_address", null: false
+    t.string "password_digest", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+  end
+
   add_foreign_key "birthday_characters", "birthday_brands"
+  add_foreign_key "sessions", "users"
 end
