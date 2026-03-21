e2ca35b18b23c486014b1399ea48b1331c2ab172
Snak-G23 <snak.gatepia@gmail.com>
2026-03-21 18:32:37 +0900

    classやdataを直接渡すように修正

--- app/components/link_component.rb
+++ app/components/link_component.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class LinkComponent < ViewComponent::Base
-  def initialize(link_text:, path:, color: "shinosawa-hiro")
+  def initialize(link_text:, path:, class_name: "color_shinosawa-hiro", data: {})
     @link_text = link_text
     @path = path
-    @color = color
+    @class_name = class_name
+    @data = data
   end
 end
