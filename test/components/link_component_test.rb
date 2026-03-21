8d9715bb47e6a89dd874fdbf5bdcb02e07756d0f
Snak <79178318+Snak0201@users.noreply.github.com>
2026-03-21 18:24:59 +0900

    Revert "セッションバーのリンクに共通コンポーネントを使えるようにする"

--- test/components/link_component_test.rb
+++ test/components/link_component_test.rb
@@ -3,15 +3,9 @@
 require "test_helper"
 
 class LinkComponentTest < ViewComponent::TestCase
-  test "should have correct link with text, href and default color" do
+  test "should have correct link with text and href" do
     render_inline(LinkComponent.new(link_text: "Example Link", path: "/"))
 
-    assert_selector "a.color_shinosawa-hiro[href='/']", text: "Example Link"
-  end
-
-  test "should have correct link with white color" do
-    render_inline(LinkComponent.new(link_text: "Example Link", path: "/", color: "white"))
-
-    assert_selector "a.color_white[href='/']", text: "Example Link"
+    assert_selector "a.link_component[href='/']", text: "Example Link"
   end
 end
