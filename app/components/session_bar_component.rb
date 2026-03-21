8e70bb50e5383e159cf02f88bcad106fca2c80e4
Snak-G23 <snak.gatepia@gmail.com>
2026-03-21 11:51:05 +0900

    cookieから認証を持ってくる

--- app/components/session_bar_component.rb
+++ app/components/session_bar_component.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class SessionBarComponent < ViewComponent::Base
+  def initialize(cookies:)
+    @cookies = cookies
+  end
+
   def authenticated?
-    Current.session.present?
+    Session.find_by(id: @cookies.signed[:session_id]) if @cookies.signed[:session_id]
   end
 end
