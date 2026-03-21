7d2e415ef92a5ca2501a831cbb13190ecadd8451
Snak-G23 <snak.gatepia@gmail.com>
2026-03-21 13:03:52 +0900

    session barへのテストを追加する

--- test/components/session_bar_component_test.rb
+++ test/components/session_bar_component_test.rb
@@ -1,12 +1,21 @@
 # frozen_string_literal: true
 
 require "test_helper"
+require "ostruct"
 
 class SessionBarComponentTest < ViewComponent::TestCase
-  def test_component_renders_something_useful
-    # assert_equal(
-    #   %(<span>Hello, components!</span>),
-    #   render_inline(SessionBarComponent.new(message: "Hello, components!")).css("span").to_html
-    # )
+  test "should have links to controls and sign out when user is signed in" do
+    session = users(:one).sessions.create!
+    cookies_mock = OpenStruct.new(signed: { session_id: session.id })
+    render_inline(SessionBarComponent.new(cookies: cookies_mock))
+    assert_selector "a", text: "管理画面"
+    assert_selector "a", text: "ログアウト"
+  end
+
+  test "should have no links when user is not signed in" do
+    cookies_mock = OpenStruct.new(signed: { session_id: nil })
+    render_inline(SessionBarComponent.new(cookies: cookies_mock))
+    assert_no_selector "a", text: "管理画面"
+    assert_no_selector "a", text: "ログアウト"
   end
 end
